733bad03a0edc15e744718c6f83c19a0f3aefa35
HOME\Andry <shapovala@ukr.net>
2017-11-02 15:18:27 +0200

    finish some sliders

--- src/js/main.js
+++ src/js/main.js
@@ -35,11 +35,46 @@
         items: 3,
         margin: 31,
         autoHeight:true,
-        nav: true
+        nav: true,
+        dots: false
     });
     /*********************************************************/
     /* end video slider  */
     /********************************************************/
+
+    /*********************************************************/
+    /* start catalog slider   */
+    /********************************************************/
+    $('.catalogList .owl-carousel').owlCarousel({
+        items: 4,
+        margin: 30,
+        autoHeight:true,
+        nav: true,
+        dots: false
+    });
+    /*********************************************************/
+    /* end catalog slider  */
+    /********************************************************/
+
+
+    /*********************************************************/
+    /* Start TABS  */
+    /********************************************************/
+    $('.tabsTitles li a').on('click', function(e){
+        e.preventDefault();
+        var $tabs = $(".tabsOuter");
+        var $tabsList = $tabs.children();
+        $(this).parent('li').addClass('tabsTitles__item--activ').siblings().removeClass('tabsTitles__item--activ');
+        var $parent = $(this).parent('li');
+        var $parentIndex = $parent.index();
+        var $current = $tabsList[$parentIndex];
+        $($current).addClass('active').siblings().removeClass('active');
+    });
+    /*********************************************************/
+    /* End TABS  */
+    /********************************************************/
+
+
 
 
 
